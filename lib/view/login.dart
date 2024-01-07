2a79ae60d1f7afc98bbf2b2576a44f17a3eb91df
yash <yash.suthar@techalchemy.com>
2024-01-07 15:04:17 +0530

    Refactor code structure for login and Implemented login u.i

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -10,52 +10,56 @@ class Login extends GetView<LoginController> {
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-
     return Scaffold(
         body: Stack(
       children: [
         Image.asset(ImageAssets.primaryBackground),
         Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: AppPadding.p15),
+            padding: const EdgeInsets.symmetric(horizontal: AppPadding.p20),
             child: Card(
               color: ColorManager.white,
               elevation: AppSize.s18,
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(AppSize.s12)),
-              child: SizedBox(
-                height: height * 0.6,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Login Account'),
-                      Text('Login with Mobile Number & M-Pin'),
-                      AppTextField(
-                          controller: controller.mobileController,
-                          focus: controller.mobileFocusNode,
-                          hintText: 'Phone Number',
-                          textInputAction: TextInputAction.next,
-                          keyboardType: TextInputType.phone,
-                          onFieldSubmitted: (_) =>
-                              FocusScope.of(context).nextFocus()),
-                      const SizedBox(height: 10),
-                      AppTextField(
-                          controller: controller.mPinController,
-                          focus: controller.mPinFocusNode,
-                          hintText: 'M-Pin',
-                          keyboardType: TextInputType.number),
-                      const SizedBox(height: 20),
-                      MaterialButton(
-                        onPressed: controller.onLogin,
-                        color: ColorManager.primary,
-                        textColor: ColorManager.white,
-                        child: const Text('Login'),
-                      )
-                    ],
-                  ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: AppPadding.p20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const SizedBox(height: 35),
+                    Image.asset(ImageAssets.appLogo),
+                    const SizedBox(height: 35),
+                    Text('Login Account',
+                        style: Theme.of(context).textTheme.titleLarge),
+                    const SizedBox(height: 15),
+                    Text('Login with Mobile Number & M-Pin',
+                        style: Theme.of(context).textTheme.titleMedium),
+                    const SizedBox(height: 30),
+                    AppTextField(
+                        controller: controller.mobileController,
+                        focus: controller.mobileFocusNode,
+                        hintText: 'Phone Number',
+                        textInputAction: TextInputAction.next,
+                        keyboardType: TextInputType.phone,
+                        onFieldSubmitted: (_) =>
+                            FocusScope.of(context).nextFocus()),
+                    const SizedBox(height: 10),
+                    AppTextField(
+                        controller: controller.mPinController,
+                        focus: controller.mPinFocusNode,
+                        hintText: 'M-Pin',
+                        keyboardType: TextInputType.number),
+                    const SizedBox(height: 20),
+                    MaterialButton(
+                      onPressed: controller.onLogin,
+                      color: ColorManager.primary,
+                      textColor: ColorManager.white,
+                      child: const Text('Login'),
+                    ),
+                    const SizedBox(height: 40),
+                  ],
                 ),
               ),
             ),
